7c503c24f729644afd35405fa47235deee08ec2c
Eugeniusz <gienas@gmail.com>
2017-05-09 22:56:18 +0200

    dasdasd

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
 
 public static void main(String... arg) {
     
-        System.out.println("Planned patch with quick unplanned change");
+        System.out.println("Next version");
 }
 
 }
