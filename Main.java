1f68447ba69f4f126c360e266d5cc37e811a6282
Eugeniusz <gienas@gmail.com>
2017-05-09 22:46:45 +0200

    v5

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
 
 public static void main(String... arg) {
     
-        System.out.println("Planned patch");
+        System.out.println("Planned patch with quick unplanned change");
 }
 
 }
