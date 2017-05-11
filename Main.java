716ab0bcac8c0f04fe3f77a4866a5e5f3639ac52
Eugeniusz <gienas@gmail.com>
2017-05-11 22:30:12 +0200

    dsdf

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
 
 public static void main(String... arg) {
     
-        System.out.println("Ala ma kota o nazwie Jery");
+        System.out.println("Ala ma kota o nazwie Jery A");
 }
 
 }
