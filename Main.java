cc26b05e1df4129d5157176df9e2085b13d0bf39
gienas <gienas@gmail.com>
2017-05-11 22:39:56 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
 public static void main(String... arg) {
    
         System.out.println("Ala ma kota o nazwie Jery");
-        System.out.println("Ala ma kota o nazwie Jery");
+        System.out.println("Ala ma kota o nazwie Jeryyyyy");
 }
 
 }
