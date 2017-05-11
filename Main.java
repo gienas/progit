7fc006aa20495697047a6f3635b652e35d95707e
Eugeniusz <gienas@gmail.com>
2017-05-11 22:35:50 +0200

    fixed

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@
 class Main {
 
 public static void main(String... arg) {
-    
-        System.out.println("Ala ma kota o nazwie Jery A");
+   
+        System.out.println("Ala ma kota o nazwie Jery");
         System.out.println("Ala ma kota o nazwie Jery");
 
 }
