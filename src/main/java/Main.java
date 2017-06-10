c23e13a100fe982a9752a68701afe90d7b612cfa
Eugeniusz <gienas@gmail.com>
2017-06-10 11:21:50 +0200

    addedone1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello my world, bug fix, bug fixedd, bug fixed !!!!!");
+		System.out.println("hello my world, bug fix, bug fixedd, bug fixed !");
 	}	
 }
