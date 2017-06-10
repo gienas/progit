1432981e506187165931a86e90fc3cd115d8e75b
Eugeniusz <gienas@gmail.com>
2017-06-10 15:00:47 +0200

    bugfix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 
-	public static void main(String... args) {
+	public static void main(String args[]) {
 		// TODO Auto-generated method stub
 		System.out.println("hello my world, bug fix, bug fixedd, bug fixed !");
 		System.out.println("Work added in branch iss01");
