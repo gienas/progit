ab40219abed2559f1fa4bbc719f724d0580bd48e
Eugeniusz <gienas@gmail.com>
2017-06-10 11:06:07 +0200

    fixedagain

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello my world, bug fix");
+		System.out.println("hello my world, bug fix, bug fixedd, bug fixed againnnnn");
 	}
 
 }
